7af09cd8c1c830857d95b7a4b365ccecae4efa40
Román Ríos <romanrios@live.com>
2026-01-30 16:10:14 -0300

    refactor: hero component renderizado desde JS

---
+++ components/Hero.js
@@ -0,0 +1,18 @@
+export function renderHero() {
+    return `
+    <div class="hero">
+      <img
+        class="hero__image"
+        src="./assets/images/perfil.png"
+        alt="Foto de perfil de Román Ríos"
+      />
+
+      <h1 class="hero__title">Román Ríos</h1>
+
+      <p class="hero__subtitle">
+        Desarrollador de Software /<br />
+        Diseñador de Comunicación Visual
+      </p>
+    </div>
+  `;
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+import { renderHero } from "./components/Hero.js";
+
+const heroSection = document.querySelector("#hero");
+heroSection.innerHTML = renderHero();

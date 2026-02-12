6d3f85b24e07a1bb1695d89d1ac6b92c97be3062
Román Ríos <romanrios@live.com>
2026-02-12 09:05:08 -0300

    profile

--- data/heroProfile.js
+++ data/heroProfile.js
@@ -25,7 +25,7 @@ Trabajo en la Oficina de Prensa del Ministerio de Trabajo, Empleo y Seguridad So
 Proactivo y adaptable, orientado al trabajo en equipo, con interés en el aprendizaje continuo y en aportar valor a cada proyecto.      `,
             en: `
 Software developer and visual communication designer, focused on web development and multimedia design.<br /><br />
-I hold a university degree in Visual Communication Design (FADU–UNL) and am currently completing a Higher Technical Degree in Software Development.<br /><br />
+I have a university background in Visual Communication Design (FADU–UNL) and am currently completing a Higher Technical Degree in Software Development.<br /><br />
 I work at the Press Office of the Ministry of Labor, Employment and Social Security of Santa Fe Province, Argentina, where I develop graphic, digital, and audiovisual communication materials for institutional projects.<br /><br />
 Proactive and adaptable, team-oriented, with a strong interest in continuous learning and in contributing value to every project.      `,
         },
